5f948aecb9c77d9f3c56a500d2b400b51adefb6d
jeferson <jeferson.santos@ilog.com.br>
2020-08-31 09:40:32 -0300

    Prettier and EsLint Config

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,18 @@
 import express from 'express';
+import route from './routes';
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-app.post('/',  (request, response) => {
+app.post('/', (request, response) => {
+  const { name, email } = request.body;
+  const user = {
+    name,
+    email,
+  };
+  return response.json(user);
+});
 
-    const {name , email} = request.body;
-    const user = {
-        name,
-        email,
-    }
-    return response.json(user);
-})
-
-
-app.listen(3333, () =>{
-    console.log('🚀 listening on port 3333')
-})
+app.listen(3333, () => {
+  console.log('🚀 listening on port 3333');
+});
