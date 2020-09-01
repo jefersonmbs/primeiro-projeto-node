eb273b456538f41cbaed5daf47495e8cfcad14bc
jeferson <jeferson.santos@ilog.com.br>
2020-09-01 19:26:53 -0300

    appointments.routs

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,9 @@
 import express from 'express';
-import route from './routes';
+import routs from './routes';
 
 const app = express();
 app.use(express.json());
-
-app.post('/', (request, response) => {
-  const { name, email } = request.body;
-  const user = {
-    name,
-    email,
-  };
-  return response.json(user);
-});
+app.use(routs);
 
 app.listen(3333, () => {
   console.log('🚀 listening on port 3333');
